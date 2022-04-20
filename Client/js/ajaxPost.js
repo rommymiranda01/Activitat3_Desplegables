4dce20c890fa4bdf098da3256f53ab9c586176e2
Rommy Miranda <rommy.miranda@cirvianum.cat>
2022-04-20 23:31:29 +0200

    commit

--- Client/js/ajaxPost.js
+++ Client/js/ajaxPost.js
@@ -1,15 +1,15 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     let ajaxClass = new AjaxClass();
 
     //Comunitats
-    ajaxClass.carregarContingut('/Server/comunitats.php','GET',null,function (text){
+    ajaxClass.carregarContingut('/Server/comunitats.php', 'GET', null, function (text) {
         let tags = text.getElementsByTagName('ca');
         let desplegable = document.getElementById('ca_name');
         let optBuit = document.createElement('option');
         optBuit.value = '-1';
         optBuit.innerText = '';
         desplegable.appendChild(optBuit);
-        for(let tag of tags){
+        for (let tag of tags) {
             let codi = tag.children[0].textContent;
             let nom = tag.children[1].textContent;
             let opt = document.createElement('option');
@@ -21,24 +21,24 @@ window.addEventListener("load",()=>{
 
     //Provincies
     document.getElementById('ca_name').addEventListener('change',
-        function (event){
+        function (event) {
             console.log(event.target.value);
             /*Caldria controlarla que no fos -1, que hauria de netejar tot el que hi ha per sota*/
 
-            ajaxClass.carregarContingut('/Server/provinciesByComunitat.php','POST','codiCom='+event.target.value,function (text){
+            ajaxClass.carregarContingut('/Server/provinciesByComunitat.php', 'POST', 'codiCom=' + event.target.value, function (text) {
                 console.log(text.getElementsByTagName('prov')[1]);
 
 
                 let provincia = document.getElementById('pr_name');
-                provincia.innerHTML="";
+                provincia.innerHTML = "";
 
                 let illa = document.getElementById('il_name');
-                illa.innerHTML="";
+                illa.innerHTML = "";
 
                 let municipi = document.getElementById('mu_name');
-                municipi.innerHTML="";
+                municipi.innerHTML = "";
 
-                for (let i=0; i<document.getElementsByClassName('hidden').length; i++){
+                for (let i = 0; i < document.getElementsByClassName('hidden').length; i++) {
                     document.getElementsByClassName('hidden')[i].classList.add("hidden")
                 }
 
@@ -50,27 +50,29 @@ window.addEventListener("load",()=>{
                 optBuit.innerText = '';
                 desplegable2.disabled = false;
                 desplegable2.appendChild(optBuit);
-                for(let tag of tags){
-                    let codiCom = tag.children[0].textContent;
+                for (let tag of tags) {
+                    let codi = tag.children[0].textContent;
                     let nom = tag.children[1].textContent;
                     let opt = document.createElement('option');
-                    opt.value = codiCom;
+                    opt.value = codi;
                     opt.innerText = nom;
                     desplegable2.appendChild(opt);
                 }
             });
+
+            //Illes
             document.getElementById('pr_name').addEventListener('change',
-                function (event){
+                function (event) {
                     console.log(event.target.value);
 
-                    ajaxClass.carregarContingut('/Server/illes.php','POST','codiProv='+event.target.value,function (text) {
-                        if (!(text.getElementsByTagName('illa').length>0)){
+                    ajaxClass.carregarContingut('/Server/illes.php', 'POST', 'codiProv=' + event.target.value, function (text) {
+                        if (!(text.getElementsByTagName('illa').length > 0)) {
 
-                            for (let i=0; i<document.getElementsByClassName('hidden').length; i++){
+                            for (let i = 0; i < document.getElementsByClassName('hidden').length; i++) {
                                 document.getElementsByClassName('hidden')[i].classList.add("hidden")
                             }
 
-                            document.getElementById("mu_name").options.length=0;
+                            document.getElementById("mu_name").options.length = 0;
                             ajaxClass.carregarContingut('/Server/municipisByProvincia.php', 'POST', 'codiProv=' + event.target.value, function (text) {
                                 let tags = text.getElementsByTagName('mun');
 
@@ -93,8 +95,10 @@ window.addEventListener("load",()=>{
                                 }
                             });
 
-                        }else {
-                            document.getElementsByClassName('hidden')[0].classList.remove("hidden");
+                        } else {
+                            for (let i = 0; i < document.getElementsByClassName('hidden').length; i++) {
+                                document.getElementsByClassName('hidden')[i].classList.remove("hidden")
+                            }
 
                             let tags = text.getElementsByTagName('illa');
                             let desplegable3 = document.getElementById('il_name');
@@ -115,6 +119,7 @@ window.addEventListener("load",()=>{
                                 desplegable3.appendChild(opt);
                             }
 
+                            //Municipis
                             document.getElementById('il_name').addEventListener('change',
                                 function (event) {
 
@@ -137,12 +142,12 @@ window.addEventListener("load",()=>{
                                             opt.innerText = nom;
                                             desplegable3.appendChild(opt);
                                         }
-                                    })
-                                })
+                                    });
+                                });
                         }
-                    })
+                    });
 
-                })
+                });
 
-        })
+        });
 });
